40243b58abbafd5a595fdec33f397fb6979f75be
Salman Shaikh <salmanshaikh18786@gmail.com>
2024-06-22 03:38:14 +0530

    contexts

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,17 +1,35 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import run from "../config/api";
 
 export const Context = createContext();
 
 const ContextProvider = (props) => {
 
+    const [input, setInput] = useState("")
+    const [recentPrompt, setRecentPrompt] = useState("")
+    const [previousPrompt, setPreviousPrompt] = useState([])
+    const [showResult, setShowResult] = useState(false)
+    const [loading, setLoading] = useState(false)
+    const [resultData, setResultData] = useState("")
+
     const onSent = async (prompt) => {
         await run(prompt)
     }
 
-    onSent("what is reactjs")
-
-  const contextValue = {};
+  const contextValue = {
+    input,
+    setInput,
+    recentPrompt,
+    setRecentPrompt,
+    previousPrompt,
+    setPreviousPrompt,
+    showResult,
+    setShowResult,
+    loading,
+    setLoading,
+    resultData,
+    setResultData
+  };
 
   return (
     <Context.Provider value={contextValue}>{props.children}</Context.Provider>
